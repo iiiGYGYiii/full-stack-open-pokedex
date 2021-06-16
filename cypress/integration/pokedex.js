c394f8f9d16757895b9bf7dc01ae43f6f96567d5
iiiGYGYiii <realGYGY@gmail.com>
2021-06-16 13:48:17 -0500

    added e2e tests

---
+++ cypress/integration/pokedex.js
@@ -0,0 +1,8 @@
+/* globals cy */
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('/')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
