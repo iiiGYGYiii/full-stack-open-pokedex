7abb656c8c4bc52f4275a2575ad94bdd1d4d61ad
iiiGYGYiii <realGYGY@gmail.com>
2021-06-16 20:27:00 -0500

    update: GHactions checks health

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000
 // random comment
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.status(200).send('ok').end()
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
